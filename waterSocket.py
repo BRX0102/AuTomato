0eaee9cee00398a4f07c390a4057f611b48ef950
salhernandez <hernandezg.sal@gmail.com>
2017-04-30 09:15:58 +0000

    emits when sensor is wet

--- waterSocket.py
+++ waterSocket.py
@@ -24,10 +24,13 @@ readings = []
 poLatitude = 36.673737
 poLongitude = -121.657529
 severity = 3
+sentFlag = False
 
 # enable the serial port for the Arduino Uno
 ser = serial.Serial('/dev/ttyACM0', 9600)
 # execute the loop forever
+
+sum = 0
 while 1:
     #water sensor
     data1 = ser.readline()
@@ -36,25 +39,27 @@ while 1:
         if int(data1) >= 100 and int(data1)<1000:
             socketIO.emit('water',data1)
             print data1
-            readings.append(int(data1))
 
             #to ping a location if the sensor reaches a certain level
             for i in readings:
                 sum = sum + int(i)
 
-            avg = int(sum / 5)
-
-            if avg < 490:
-                print "below the threshold"
-                socketIO.emit({
-                    'latitude': poLatitude,
-                    'longitude': poLongitude,
-                    'blockType': severity,
-                })
+            
+            
+            if int(data1) < 490:
+                if sentFlag is not True: 
+                    print "below the threshold"
+                    socketIO.emit('markEndPoint',{
+                        'latitude': poLatitude,
+                        'longitude': poLongitude,
+                        'blockType': severity,
+                    })
+                    sentFlag = True
 
             print readings
             if len(readings) == 5:
                 # deletes the list
                 del readings[:]
+                sum = 0
 
-    socketIO.wait(seconds=1)
+    #socketIO.wait(seconds=1)
