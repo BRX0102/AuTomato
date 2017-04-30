117cdd9a2539740914f7bd5b1746d8684f1ae728
Brandan Lockwood <blockwood@csumb.edu>
2017-04-30 06:21:28 +0000

    heroku

--- static/script.js
+++ static/script.js
@@ -31170,6 +31170,77 @@ module.exports = __webpack_amd_options__;
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(30);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _googleMapReact = __webpack_require__(256);
+
+var _googleMapReact2 = _interopRequireDefault(_googleMapReact);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var AnyReactComponent = function AnyReactComponent(_ref) {
+  var text = _ref.text;
+  return _react2.default.createElement(
+    'div',
+    { style: {
+        position: 'relative', color: 'white', background: 'red',
+        height: 40, width: 60, top: -20, left: -30
+      } },
+    text
+  );
+};
+
+var SimpleMap = function (_Component) {
+  _inherits(SimpleMap, _Component);
+
+  function SimpleMap() {
+    _classCallCheck(this, SimpleMap);
+
+    return _possibleConstructorReturn(this, (SimpleMap.__proto__ || Object.getPrototypeOf(SimpleMap)).apply(this, arguments));
+  }
+
+  _createClass(SimpleMap, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        _googleMapReact2.default,
+        {
+          defaultCenter: this.props.center,
+          defaultZoom: this.props.zoom
+        },
+        _react2.default.createElement(AnyReactComponent, {
+          lat: 59.955413,
+          lng: 30.337844,
+          text: 'Kreyser Avrora'
+        })
+      );
+    }
+  }]);
+
+  return SimpleMap;
+}(_react.Component);
+
+exports.default = SimpleMap;
+
+SimpleMap.defaultProps = {
+  center: { lat: 59.95, lng: 30.33 },
+  zoom: 11
+};
+
 /***/ }),
 /* 247 */
 /***/ (function(module, exports, __webpack_require__) {
